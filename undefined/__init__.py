f283b8433414d95d1b2ad22b92b35511b8585b40
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-12-22 21:42:57 +0100

    polish package a bit

--- undefined/__init__.py
+++ undefined/__init__.py
@@ -6,4 +6,30 @@ a simple package
 
 __version__ = '0.0.1'
 
-        
+import sys
+import types
+
+class Undefined(types.ModuleType):
+
+
+    @property
+    def Undefined(self):
+        return self
+
+    def __call__(self, value):
+        return value is self
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    __str__ = __repr__
+
+
+Undefined.__name__ = 'Undefined'
+Undefined = Undefined('undefined')
+
+if sys.modules[__name__] is Undefined:
+    print('doing nothing')
+    pass
+else:
+   sys.modules[__name__] = Undefined
---
+++ undefined/tests/test_undefined.py
@@ -0,0 +1,7 @@
+import undefined as ud
+from  undefined import Undefined as uc
+
+
+def test_undefined():
+    assert ud is uc
+
